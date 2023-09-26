17066748e876d076852773a5f0c7e689d72f7301
Kevin <kevsalvatierrafs@gmail.com>
2023-09-26 11:04:39 -0300

    Ajuste en la indentacion para mejorar legibilidad

--- src/main/java/org/generation/cyberpunk/models/Estudiante.java
+++ src/main/java/org/generation/cyberpunk/models/Estudiante.java
@@ -1,6 +1,5 @@
 package org.generation.cyberpunk.models;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
