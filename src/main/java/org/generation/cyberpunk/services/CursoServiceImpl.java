3ccfcef0f147be10c589ea33be53fe061549a4eb
Kevin <kevsalvatierrafs@gmail.com>
2023-09-26 11:47:27 -0300

    Agregado el metodo para editar un curso por id

--- src/main/java/org/generation/cyberpunk/services/CursoServiceImpl.java
+++ src/main/java/org/generation/cyberpunk/services/CursoServiceImpl.java
@@ -24,7 +24,7 @@ public class CursoServiceImpl implements CursoService {
 
     @Override
     public void borrarCurso(Long id) {
-        cursoRepository.deleteById(id);
+            cursoRepository.deleteById(id);
     }
 
     @Override
@@ -40,6 +40,16 @@ public class CursoServiceImpl implements CursoService {
 
     @Override
     public Curso editarCursoPorId(Long id, Curso curso) {
+        if (cursoRepository.existsById(id) && cursoRepository.findById(id).isPresent()) {
+            Curso nuevoCurso = cursoRepository.findById(id).get();
+            nuevoCurso.setNombre(curso.getNombre());
+            nuevoCurso.setProfesor(curso.getProfesor());
+            nuevoCurso.setTema(curso.getTema());
+            nuevoCurso.setFecha(curso.getFecha());
+
+            return cursoRepository.save(nuevoCurso);
+        }
+        System.out.println("No se encuentra un curso con ese ID, intentalo otra vez.");
         return null;
     }
 }
